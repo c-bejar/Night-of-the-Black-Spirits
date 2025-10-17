0f9ced54d884d247420a17992e9af3a5386a0883
Christopher Bejar <p.bejar.christopher@gmail.com>
2025-10-17 00:37:35 -0500

    add: tv destruction when hit with axe

--- Areas/House/house.gd
+++ Areas/House/house.gd
@@ -4,6 +4,7 @@ const tv_scene: PackedScene = preload("res://Entities/TV/tv.tscn")
 
 var total_tvs: int = 0
 var spawns: Array = []
+var tvs_in_range: Array = []
 
 func _ready() -> void:
 	var children: Array = $"SpawnPoints".get_children()
@@ -17,11 +18,16 @@ func _ready() -> void:
 		var tv_instance: StaticBody2D = tv_scene.instantiate()
 		tv_instance.position = spawn
 		$".".add_child(tv_instance)
-	
 
-func _process(_delta: float) -> void:
-	pass
+func _on_player_axe_attack() -> void:
+	for tv: StaticBody2D in tvs_in_range:
+		if "tv_destroyed" in tv:
+			tv.tv_destroyed()
 
-func get_new_spawn() -> void:
-	pass
-	
+
+func _on_player_tv_entered(body: Node2D) -> void:
+	tvs_in_range.append(body)
+
+
+func _on_player_tv_exited(body: Node2D) -> void:
+	tvs_in_range.erase(body)
