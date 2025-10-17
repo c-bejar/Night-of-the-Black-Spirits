9aa5681c90946e413bcbf2d7b7c7bd2f27ca2697
Christopher Bejar <p.bejar.christopher@gmail.com>
2025-10-17 15:54:03 -0500

    add: spawn tv when one is destroyed

--- Areas/House/house.gd
+++ Areas/House/house.gd
@@ -1,27 +1,39 @@
 extends Node2D
 
-const tv_scene: PackedScene = preload("res://Entities/TV/tv.tscn")
+const TVScene: PackedScene = preload("res://Entities/TV/tv.tscn")
+const MAX_TVS: int = 6
 
 var total_tvs: int = 0
 var spawns: Array = []
 var tvs_in_range: Array = []
 
 func _ready() -> void:
+	spawn_tvs()
+
+
+func _process(_delta: float) -> void:
+	if total_tvs < MAX_TVS:
+		spawn_tvs()
+
+
+func spawn_tvs() -> void:
 	var children: Array = $"SpawnPoints".get_children()
-	while total_tvs < 6:
-		var rand_val: int = randi() % children.size()
+	var total_locs: int = children.size()
+	while total_tvs < MAX_TVS:
+		var rand_val: int = randi() % total_locs
 		if children[rand_val].global_position not in spawns:
 			spawns.append(children[rand_val].global_position)
+			var TVInstance: StaticBody2D = TVScene.instantiate()
+			TVInstance.global_position = children[rand_val].global_position
+			$".".add_child(TVInstance)
 			total_tvs += 1
-	print(spawns)
-	for spawn: Vector2 in spawns:
-		var tv_instance: StaticBody2D = tv_scene.instantiate()
-		tv_instance.position = spawn
-		$".".add_child(tv_instance)
+
 
 func _on_player_axe_attack() -> void:
 	for tv: StaticBody2D in tvs_in_range:
 		if "tv_destroyed" in tv:
+			spawns.erase(tv.global_position)
+			total_tvs -= 1
 			tv.tv_destroyed()
 
 
