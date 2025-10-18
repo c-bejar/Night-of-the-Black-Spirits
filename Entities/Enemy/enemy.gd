32db1ca59f175c2edf583b3aec965a4ad2b44161
Christopher Bejar <p.bejar.christopher@gmail.com>
2025-10-17 19:05:40 -0500

    add: health and hit functions

--- Entities/Enemy/enemy.gd
+++ Entities/Enemy/enemy.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 const SPEED: int = 25
 
+var health: int = 5
+
 func _process(delta: float) -> void:
 	self.look_at(Globals.player_pos)
 	
@@ -12,3 +14,15 @@ func _process(delta: float) -> void:
 
 	if collision:
 		pass
+
+
+func hit() -> void:
+	$GPUParticles2D.material.set_shader_parameter("progress", 1)
+	$ShaderTimer.start()
+	health -= 1
+	if health <= 0:
+		self.queue_free()
+
+
+func _on_shader_timer_timeout() -> void:
+	$GPUParticles2D.material.set_shader_parameter("progress", 0)
