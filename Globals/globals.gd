e167660719a65c7f18dc5d8f7364bca42fc5f761
Christopher Bejar <p.bejar.christopher@gmail.com>
2025-10-17 19:03:01 -0500

    change: moved spawns to globals; add: timer tween to end game

--- Globals/globals.gd
+++ Globals/globals.gd
@@ -1,3 +1,9 @@
 extends Node
 
+var spawns: Array = []
 var player_pos: Vector2 = Vector2.ZERO
+var timer_amount: float = 10.0
+
+func _ready() -> void:
+	var tween: PropertyTweener = self.create_tween().tween_property(self, "timer_amount", 0, 300)
+	tween.set_trans(Tween.TRANS_CIRC)
