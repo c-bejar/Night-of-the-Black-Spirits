37a139f7f12d06e5d6970c2a64888246f32a7362
Christopher Bejar <p.bejar.christopher@gmail.com>
2025-10-19 14:54:20 -0500

    change: play score sfx and tween colors for score text; add: health bar update function

--- Globals/ui.gd
+++ Globals/ui.gd
@@ -2,7 +2,23 @@ extends CanvasLayer
 
 func _ready() -> void:
 	Globals.update_score.connect(update_score_text)
+	Globals.update_health.connect(update_health_bar)
 	update_score_text()
+	update_health_bar()
 
-func update_score_text() -> void:
-	%ScoreBox.text = str(Globals.current_score)
+func update_score_text(collected: bool = false) -> void:
+	if collected:
+		$Audio/ScoreUp.stop()
+		$Audio/ScoreUp.play()
+		var tween: Tween = self.create_tween()
+		tween.tween_property(%HBoxContainer, "modulate", Color(1.0, 1.0, 1.0, 1.0), 0.15)
+		tween.tween_property(%HBoxContainer, "modulate", Color(0.0, 0.784, 0.0, 1.0), 0.15)
+
+	%ScoreBox.text = "%09d" % Globals.current_score
+func update_health_bar(_damaged: bool = false) -> void:
+	#if damaged:
+		#var tween: Tween = self.create_tween()
+		#tween.tween_property(%ProgressBar, "bg_color", Color(0.608, 0.0, 0.0, 1.0), 0.15)
+		#tween.tween_property(%ProgressBar, "bg_color", Color(0.196, 0.549, 0.196, 1.0), 0.15)
+	%ProgressBar.value = Globals.player_health
+	
