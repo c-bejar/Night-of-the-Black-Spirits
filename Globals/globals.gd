342c861063276fcc64e5f3a3e1ba5ac526eef089
Christopher Bejar <p.bejar.christopher@gmail.com>
2025-10-19 01:21:34 -0500

    add: score and signal for ui change; change: reduced timer for debug purposes

--- Globals/globals.gd
+++ Globals/globals.gd
@@ -1,8 +1,18 @@
 extends Node
 
+signal update_score()
+
+var current_score: int = 0:
+	set(value):
+		if value > 999_999_999:
+			current_score = 999_999_999
+		else:
+			current_score = value
+		update_score.emit()
+var highest_score: int = 0
 var spawns: Array = []
 var player_pos: Vector2 = Vector2.ZERO
-var timer_amount: float = 10.0
+var timer_amount: float = 1.0
 
 func _ready() -> void:
 	var tween: PropertyTweener = self.create_tween().tween_property(self, "timer_amount", 0, 300)
