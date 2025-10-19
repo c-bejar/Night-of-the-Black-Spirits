5ce480bc5c776a31bf68d0489c3909ee16e85eef
Christopher Bejar <p.bejar.christopher@gmail.com>
2025-10-18 22:51:48 -0500

    add: ability to attack

--- Entities/Enemy/enemy.gd
+++ Entities/Enemy/enemy.gd
@@ -3,8 +3,14 @@ extends CharacterBody2D
 const SPEED: int = 25
 
 var health: int = 5
+var currently_attacking: bool = false
+var PlayerBody: CharacterBody2D = null
 
 func _process(delta: float) -> void:
+	if currently_attacking:
+		if "hit" in PlayerBody:
+			PlayerBody.hit()
+		
 	self.look_at(Globals.player_pos)
 	
 	var direction: Vector2 = (Globals.player_pos - self.global_position).normalized()
@@ -17,6 +23,7 @@ func _process(delta: float) -> void:
 
 
 func hit() -> void:
+	$HurtSound.play()
 	$GPUParticles2D.material.set_shader_parameter("progress", 1)
 	$ShaderTimer.start()
 	health -= 1
@@ -26,3 +33,13 @@ func hit() -> void:
 
 func _on_shader_timer_timeout() -> void:
 	$GPUParticles2D.material.set_shader_parameter("progress", 0)
+
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	currently_attacking = true
+	PlayerBody = body
+
+
+func _on_attack_area_body_exited(_body: Node2D) -> void:
+	currently_attacking = false
+	PlayerBody = null
