6ed06fb5ace9912608f2145ecd595436e34f4b3e
Christopher Bejar <p.bejar.christopher@gmail.com>
2025-10-19 19:37:47 -0500

    add: functionality for ui via button presses and sfx

--- Globals/ui.gd
+++ Globals/ui.gd
@@ -3,8 +3,14 @@ extends CanvasLayer
 func _ready() -> void:
 	Globals.update_score.connect(update_score_text)
 	Globals.update_health.connect(update_health_bar)
+	Globals.game_has_ended.connect(end_game_process)
 	update_score_text()
 	update_health_bar()
+	
+
+func start_game() -> void:
+	$GameUI.show()
+
 
 func update_score_text(collected: bool = false) -> void:
 	if collected:
@@ -15,10 +21,29 @@ func update_score_text(collected: bool = false) -> void:
 		tween.tween_property(%HBoxContainer, "modulate", Color(0.0, 0.784, 0.0, 1.0), 0.15)
 
 	%ScoreBox.text = "%09d" % Globals.current_score
+	
+	
 func update_health_bar(_damaged: bool = false) -> void:
-	#if damaged:
-		#var tween: Tween = self.create_tween()
-		#tween.tween_property(%ProgressBar, "bg_color", Color(0.608, 0.0, 0.0, 1.0), 0.15)
-		#tween.tween_property(%ProgressBar, "bg_color", Color(0.196, 0.549, 0.196, 1.0), 0.15)
 	%ProgressBar.value = Globals.player_health
 	
+
+func end_game_process() -> void:
+	$GameUI.hide()
+	$EndUI.show()
+	%FinalScoreBox.text = "%09d" % Globals.current_score
+	$Audio/EndAudio.volume_db = 0
+
+
+func _on_start_button_pressed() -> void:
+	self.get_tree().change_scene_to_file("res://Areas/House/house.tscn")
+	$StartUI.hide()
+	$GameUI.show()
+	$Audio/MenuAudio.volume_db = -80
+
+
+func _on_controls_button_pressed() -> void:
+	$StartUI/ControlsPopup.show()
+
+
+func _on_controls_exit_button_pressed() -> void:
+	$StartUI/ControlsPopup.hide()
